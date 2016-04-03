7751d39a082743fff77eaf0bc1a77f25fc2f5daa
AnjanDutta <duttanjan@gmail.com>
2016-04-03 12:44:01 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# CourseraGettingCleaningDataProject
+# Coursera Getting and Cleaning Data Project
+
+This repository contains the R code, documentation files and the resulting tidy data set of the Coursera "Getting and Cleaning data" project.
+
+The dataset being used is: [Human Activity Recognition Using Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+## Files
+
+1. `run_analysis.R` contains all the code to perform the analyses described in the 5 steps. They can be launched in RStudio by just importing the file. 
+  * The code downloads and unzips the data in the same folder
+  * It then performs the subsequent data analysis from step one to step five
+
+2. `CodeBook.md` describes the variables, the data, and any transformations or work that was performed to clean up the data.
+
+3. The output of the 5th step is called `averages_data.txt`, and uploaded in the course project's form.
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+###############################################################################
 # R script called run_analysis.R that does the following:
 # 
 # 1. Merges the training and the test sets to create one data set.
@@ -5,14 +6,17 @@
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+###############################################################################
 
 library(plyr)
+
+#Step 0: Prepare the working dierectory and data
 
 setwd("/home/dag/Workspace/DataScienceCoursera/CleaningData/week4/Project")
 
 filename <- "UCI HAR Dataset.zip"
 
-## Download and unzip the dataset:
+# Download and unzip the dataset:
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
   download.file(fileURL, filename, method="curl")
